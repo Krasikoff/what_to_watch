00fba15d7ac8601b209351c514bff5ce4f464ce8
Красиков Дмитрий <z1r0-d@yandex.ru>
2023-12-26 18:47:38 +0300

    Error after send WTForm.

--- opinions_app.py
+++ opinions_app.py
@@ -1,17 +1,22 @@
 # what_to_watch/opinions_app.py
-
 from datetime import datetime
 
 # Импортируется функция выбора случайного значения
 from random import randrange
 
+
+from wtforms import StringField, SubmitField, TextAreaField, URLField
+from wtforms.validators import DataRequired, Length, Optional
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = '#!@HD(Clkxzcv89)'
 
 db = SQLAlchemy(app)
 
@@ -22,6 +27,23 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+
+class OpinionForm(FlaskForm):
+    title = StringField(
+        'Введите название фильма',
+        validators=[DataRequired(message='Обязательное поле'),
+                    Length(1, 128)]
+    )
+    text = TextAreaField(
+        'Напишите мнение', 
+        validators=[DataRequired(message='Обязательное поле')]
+    )
+    source = URLField(
+        'Добавьте ссылку на подробный обзор фильма',
+        validators=[Length(1, 256), Optional()]
+    )
+    submit = SubmitField('Добавить')
 
 
 @app.route('/')
@@ -36,12 +58,23 @@ def index_view():
     offset_value = randrange(quantity)
     # И определяется случайный объект
     opinion = Opinion.query.offset(offset_value).first()
-    return render_template('index.html', opinion=opinion )
+#    opinion = Opinion.query.get_or_404(id)    
+    return render_template('opinion.html', opinion=opinion )
 
 
 @app.route('/add')
 def add_opinion_view():
-    return 'Страница в разработке!'
+    # Вот тут создаётся новый экземпляр формы
+    form = OpinionForm()  
+    return render_template('add_opinion.html', form=form)
+
+@app.route('/opinions/<int:id>')  
+# Параметром указывается имя переменной
+def opinion_view(id):  
+    # Теперь можно запрашивать мнение по id
+    opinion = Opinion.query.get_or_404(id)
+    # И передавать его в шаблон
+    return render_template('opinion.html', opinion=opinion)
 
 
 if __name__ == '__main__':
